998ca2e4ba18afee005de44e4edabfed1d1bc042
Jorgen Jensvold <jorgenjensvold@gmail.com>
2023-02-08 19:55:30 +0000

    Add bash script folder from my system for backup

---
+++ temps.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+gpu_temps=($(cat /sys/class/thermal/thermal_zone*/temp))
+
+echo GPU Temps in Celsius:
+echo \  $(awk '{print $1/1000}' <<< "${gpu_temps[0]}") 
+echo \  $(awk '{print $1/1000}' <<< "${gpu_temps[1]}") 
+echo \  $(awk '{print $1/1000}' <<< "${gpu_temps[2]}") 
+
+
+cpu_cores=6
+
+function cpu_temps {
+    i=0
+    echo \  $(sensors | grep "Package")
+    while [ $i -lt $cpu_cores ]
+    do
+        echo \  \  $(sensors | grep "Core ${i}")
+        ((i++))
+    done
+}
+
+echo 
+echo CPU temps:
+cpu_temps
